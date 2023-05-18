6cbc4939655b1b60414742ab59d9101bcd4aae1c
박세준 <sjpark@selab.co.kr>
2023-05-18 17:24:49 +0900

    임시

--- src/main/java/com/roles/rbacserver/accountrole/application/AccountRoleService.java
+++ src/main/java/com/roles/rbacserver/accountrole/application/AccountRoleService.java
@@ -24,18 +24,30 @@ public class AccountRoleService {
     private final ApplicationContext applicationContext;
     protected static final Map<String, Map<String, Set<AccountRole>>> METHOD_URI_ACCOUNT_ROLE_MAP = new HashMap<>();
 
+    /**
+     * AccountRoleService 생성자
+     *
+     * @param applicationContext 스프링 애플리케이션 컨텍스트
+     */
     public AccountRoleService(ApplicationContext applicationContext) {
         this.applicationContext = applicationContext;
     }
 
+    /**
+     * URIAndAccountRole 처리하기 위한 함수형 인터페이스
+     * processRestControllers 를 통해 나온 값들을 apply 하여서 값을 구하거나 METHOD_URI_ACCOUNT_ROLE_MAP 에 값을 넣어준다.
+     */
     @FunctionalInterface
     private interface AccountRoleOperator {
         void apply(Method method, List<String> classBaseURIs);
     }
-
+    /**
+     * 컨텍스트가 로드된 후, URI 와 권한 역할 매핑해준다
+     * 모든 @RestController 의 URI 들을 @NeedAccountRole 이 붙은 경우 어노테이션의 벨류를 어노테이션이 붙지 않은 경우 AccountRole.ALL 을 추가하여서 권한을 전부 풀어준다.
+     */
     @EventListener(ContextRefreshedEvent.class)
     public void initURIAndAccountRole() {
-        processControllers((method, classBaseURIs) -> {
+        processRestControllers((method, classBaseURIs) -> {
             Map<String, String> methodURIAndRoleMap = extractMethodURIAndRole(method, ".+");
             Set<AccountRole> accountRoleSet = EnumSet.of(AccountRole.ALL);
             if (method.isAnnotationPresent(NeedAccountRole.class)) {
@@ -59,9 +71,14 @@ public class AccountRoleService {
         });
     }
 
+    /**
+     * AccountRole 어노테이션이 붙은 URI를 찾음
+     *
+     * @return AccountRole 어노테이션이 붙은 URI 목록
+     */
     public List<String> findURIAnnotatedNeedAccountRole() {
         List<String> annotatedURIs = new ArrayList<>();
-        processControllers((method, classBaseURIs) -> {
+        processRestControllers((method, classBaseURIs) -> {
             Map<String, String> methodURIAndRoleMap = extractMethodURIAndRole(method, "**");
             if (method.isAnnotationPresent(NeedAccountRole.class)) {
                 for (String classBaseURI : classBaseURIs) {
@@ -74,6 +91,13 @@ public class AccountRoleService {
         return annotatedURIs;
     }
 
+    /**
+     * 주어진 URI에 대한 권한 역할 정보를 가져옴
+     *
+     * @param method HTTP 메소드 (GET, POST, etc.)
+     * @param uri 요청 URI
+     * @return 해당 URI에 대한 AccountRole 집합
+     */
     public Set<AccountRole> getRoleInfoByURI(String method, String uri) {
         Map<String, Set<AccountRole>> uriAccountROleMap = METHOD_URI_ACCOUNT_ROLE_MAP.get(method);
         if (uriAccountROleMap == null) {
@@ -86,12 +110,25 @@ public class AccountRoleService {
         return uriAccountROleMap.get(matchedUri);
     }
 
+    /**
+     * 패턴들 중에 URI와 가장 잘 매칭되는 패턴을 반환
+     *
+     * @param uri 입력 URI
+     * @param patterns 비교를 위한 패턴들의 집합
+     * @return 가장 잘 매칭되는 패턴(Optional)
+     */
     private static Optional<String> getBestMatchingPattern(String uri, Set<String> patterns) {
         return patterns.stream()
                 .filter(uri::matches)
                 .max(Comparator.comparingInt(String::length));
     }
 
+    /**
+     * 클래스 기반 URI를 가져옴
+     *
+     * @param classRequestMapping 클래스에 붙은 RequestMapping 어노테이션
+     * @return 클래스 기반 URI 목록
+     */
     private List<String> getClassBaseURIs(RequestMapping classRequestMapping) {
         if (classRequestMapping != null && classRequestMapping.value().length > 0) {
             return Arrays.asList(classRequestMapping.value());
@@ -99,6 +136,13 @@ public class AccountRoleService {
         return new ArrayList<>();
     }
 
+    /**
+     * 주어진 메소드에서 URI와 역할을 추출
+     *
+     * @param method 메소드
+     * @param replacement URI 패턴에서 치환할 값
+     * @return URI와 메소드의 맵
+     */
     private Map<String, String> extractMethodURIAndRole(Method method, String replacement) {
         Map<String, String> urisAndMethod = new HashMap<>();
 
@@ -125,7 +169,12 @@ public class AccountRoleService {
         return urisAndMethod;
     }
 
-    private void processControllers(AccountRoleOperator operator) {
+    /**
+     * 어토네이션 @RestController 이 붙은 컨트롤러들 전부 가져와서 RequestMapping 정보와 함께 HTTP Method, URI 등을 전달한다.
+     *
+     * @param operator 적용할 AccountRoleOperator
+     */
+    private void processRestControllers(AccountRoleOperator operator) {
         Map<String, Object> restControllers = applicationContext.getBeansWithAnnotation(RestController.class);
         for (Object restController : restControllers.values()) {
             Class<?> restControllerClass = restController.getClass();
@@ -139,6 +188,13 @@ public class AccountRoleService {
         }
     }
 
+    /**
+     * 주어진 값에서 URI 를 추출
+     * 인터셉터에 등록할 값은 /** 으로 패턴으로 비교할 값은 /.+으로 비교한다. replacement에 이 값을 담아 전달
+     * @param values 추출을 위한 입력 값
+     * @param replacement URI 패턴에서 치환할 값
+     * @return 추출된 URI 목록
+     */
     private List<String> extractUrisFromValue(String[] values, String replacement) {
         if (values == null || values.length == 0) {
             return Collections.singletonList("");
