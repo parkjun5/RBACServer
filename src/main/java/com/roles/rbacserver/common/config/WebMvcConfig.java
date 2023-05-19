783df3070eb40e4762ebbe08429b9bb11e9122c9
박세준 <sjpark@selab.co.kr>
2023-05-19 11:10:49 +0900

    수정

--- src/main/java/com/roles/rbacserver/common/config/WebMvcConfig.java
+++ src/main/java/com/roles/rbacserver/common/config/WebMvcConfig.java
@@ -1,6 +1,7 @@
 package com.roles.rbacserver.common.config;
 
 import com.roles.rbacserver.accountrole.application.AccountRoleService;
+import com.roles.rbacserver.accountrole.application.AccountRoleServiceLikeSpring;
 import com.roles.rbacserver.common.interceptor.RoleBaseAccessInterceptor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
@@ -9,18 +10,17 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
 
-    private final AccountRoleService accountRoleFinder;
+    private final AccountRoleService accountRoleService;
     private final RoleBaseAccessInterceptor roleBaseAccessInterceptor;
 
-    public WebMvcConfig(AccountRoleService accountRoleFinder, RoleBaseAccessInterceptor roleBaseAccessInterceptor) {
-        this.accountRoleFinder = accountRoleFinder;
+    public WebMvcConfig(AccountRoleService accountRoleService, RoleBaseAccessInterceptor roleBaseAccessInterceptor) {
+        this.accountRoleService = accountRoleService;
         this.roleBaseAccessInterceptor = roleBaseAccessInterceptor;
     }
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(roleBaseAccessInterceptor)
-                //TODO annotaion Matcher
-                .addPathPatterns(accountRoleFinder.findURIAnnotatedNeedAccountRole());
+                .addPathPatterns(accountRoleService.findURIAnnotatedNeedAccountRole());
     }
 }
